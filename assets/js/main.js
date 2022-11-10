0733704fd6b759df49d38044f06e6501ad1bccdf
Rafael Diaz <rafaelmarzoratti02@gmail.com>
2022-11-10 11:18:53 -0300

    main.js adicionado e style modificado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("Ola gurizes")
+let email = document.getElementById("email")
+let password = document.getElementById("password")
+
+email.addEventListener("focus", () => {
+  email.style.borderColor = "#16425a"
+})
+
+email.addEventListener("blur", () => {
+  email.style.borderColor = "#ccc"
+})
+
+password.addEventListener("focus", () => {
+  password.style.borderColor = "#16425a"
+})
+
+password.addEventListener("blur", () => {
+  password.style.borderColor = "#ccc"
+})
